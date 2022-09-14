d13d0e56b155f57f34c6d052815932f9a0cd9cdb
jjininu <jjininu@gmail.com>
2022-09-13 20:44:04 -0400

    update

--- init_setup.sh
+++ init_setup.sh
@@ -1,3 +1,8 @@
+# this is to create env and install all the req, instaid of running pip install on terminal
+# echo is just to print a massage 
+# creating such script with python script can result an issue , so thats why shell script is used
+# it has to be run in gitbash terminal
+
 echo [$(date)]: "START" 
 echo [$(date)]: "creating env with python 3.8 version" 
 conda create --prefix ./env python=3.8 -y
